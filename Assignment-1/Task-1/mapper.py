4152f524ab717372e986830ff0774915ac0179b4
nsankethreddy <sankethn1947@gmail.com>
2020-09-02 15:17:30 +0530

    Add is_weekend function

--- Assignment-1/Task-1/mapper.py
+++ Assignment-1/Task-1/mapper.py
@@ -5,11 +5,14 @@ import datetime
 f = open("../../../plane_carriers.ndjson","r")
 lines = f.readlines()
 
-def is_weekend(i):
-    return 1
-# print (lines[1])
+def is_weekend(date):
+    day = datetime.datetime.strptime(date, '%Y-%m-%d').weekday()
+    if (day == 5 or day == 6):
+        return 1
+    else:
+        return 0
+
 for line in lines:
-    # line = lines[1];
     i = 0
     finallist = []
     line = line.strip().strip("{").strip("}").split(", ")
@@ -17,10 +20,11 @@ for line in lines:
         x = (line[i].split(":")[1]).strip(" ").strip('"')
         finallist.append(str(x))
         i +=1
-    print (finallist)
     if finallist[0] == "aircraft carrier":
         if (finallist[3] == 'true'):
             print(finallist[0],1)
+            pass
         
         elif (finallist[3] == 'false') & is_weekend(finallist[2].split()[0]):
             print(finallist[0],2)
+            pass
