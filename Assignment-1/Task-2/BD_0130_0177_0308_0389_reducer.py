eb29b40de8b3a858560dabd61f3404e0c7ce4b3a
Akshatha Laxmi <akshata.lakshmi@gmail.com>
2020-09-04 15:10:04 +0530

    Reducer 2

---
+++ Assignment-1/Task-2/BD_0130_0177_0308_0389_reducer.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python3
+"""reducer.py"""
+
+import sys
+
+current_word = None
+current_count = 0
+word = None
+
+for line in sys.stdin:
+    line = line.strip()
+    word, count = line.split()
+
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            print(current_word, current_count, sep=",")
+        current_count = count
+        current_word = word
+
+if current_word == word:
+    print(current_word, current_count, sep=",")
