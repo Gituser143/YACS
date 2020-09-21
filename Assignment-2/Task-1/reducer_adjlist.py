33619f5cc50dd4bece0a9b5be6f49db1c65389a8
Gituser143 <bhargavsnv100@gmail.com>
2020-09-22 00:44:16 +0530

    Remove extra comment check and added try in adj list reducer

--- Assignment-2/Task-1/reducer_adjlist.py
+++ Assignment-2/Task-1/reducer_adjlist.py
@@ -4,19 +4,21 @@ import sys
 current_node = None
 outlinks = []
 node = None
+
 for line in sys.stdin:
     line = line.strip()
-    if line != '':
-        if line[0] != '#':
-            node, outlink = line.split()
-            if current_node == node:
-                outlinks.append(outlink)
-            else:
-                if current_node:
-                    comma_sep_value = ",".join(outlinks)
-                    print(current_node, comma_sep_value)
-                outlinks = [outlink]
-                current_node = node
+    node, outlink = line.split()
+    try:
+        if current_node == node:
+            outlinks.append(outlink)
+        else:
+            if current_node:
+                comma_sep_value = ",".join(outlinks)
+                print(current_node, comma_sep_value)
+            outlinks = [outlink]
+            current_node = node
+    except:
+        continue
 
 if current_node == node:
     comma_sep_value = ",".join(outlinks)
