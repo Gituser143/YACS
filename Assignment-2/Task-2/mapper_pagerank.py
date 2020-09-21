ab6db3046438e87d5d41bf01b410af7b6c19458f
Gituser143 <bhargavsnv100@gmail.com>
2020-09-21 22:21:38 +0530

    Fix bad formatting

--- Assignment-2/Task-2/mapper_pagerank.py
+++ Assignment-2/Task-2/mapper_pagerank.py
@@ -2,17 +2,16 @@
 import sys
 
 for line in sys.stdin:
-        node, outlinks = line.split()
-        outlinks = outlinks.split(",")
-        outlinks = [int(i) for i in outlinks]
-        with open("v.txt", "r") as file:
-            while True:
-                file_line = file.readline().strip()
-                v_node, rank = file_line.split(",")
-                if v_node == node:
-                    for i in outlinks:
-                        print(i, rank/len(outlinks))
+    node, outlinks = line.split()
+    outlinks = outlinks.split(",")
+    outlinks = [int(i) for i in outlinks]
+    with open("v.txt", "r") as file:
+        while True:
+            file_line = file.readline().strip()
+            v_node, rank = file_line.split(",")
+            if v_node == node:
+                for i in outlinks:
+                    print(i, rank/len(outlinks))
                     break
-                if not file_line:
-                    break
-
+            if not file_line:
+                break
