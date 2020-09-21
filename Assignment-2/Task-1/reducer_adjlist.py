35767fae7d410bc66b9040d29ea537174518bf5c
Gituser143 <bhargavsnv100@gmail.com>
2020-09-22 00:39:46 +0530

    Change reducer_adjlist to not hold list in memory.

--- Assignment-2/Task-1/reducer_adjlist.py
+++ Assignment-2/Task-1/reducer_adjlist.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 import sys
 
-dict = {}
-
+current_node = None
+outlinks = []
+node = None
 for line in sys.stdin:
     line = line.strip()
     if line != '':
         if line[0] != '#':
-            try:
-                key, value = line.split()
-                if key not in dict:
-                    dict[key] = []
-                    dict[key].append(int(value))
-                elif key in dict:
-                    dict[key].append(int(value))
-            except:
-                continue
+            node, outlink = line.split()
+            if current_node == node:
+                outlinks.append(outlink)
+            else:
+                if current_node:
+                    comma_sep_value = ",".join(outlinks)
+                    print(current_node, comma_sep_value)
+                outlinks = [outlink]
+                current_node = node
 
-for key, value in dict.items():
-    print(key, end=" ")
-    str_value = [str(i) for i in value]
-    comma_sep_value = ",".join(str_value)
-    print(comma_sep_value)
+if current_node == node:
+    comma_sep_value = ",".join(outlinks)
+    print(current_node, comma_sep_value)
