529d4ab61f2c1721642444144e56573adcb6eac3
Abhishek262 <abhishek262014@gmail.com>
2020-09-21 15:44:52 +0530

    added basic mapper and reducer to output adjacency list

---
+++ Assignment-2/Task-1/mapper_adjlist.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" mapper.py """
+
+import sys
+# Read command line args
+# word = sys.argv[1]
+
+
+for line in sys.stdin:
+
+    try:
+        print(line)
+        
+
+    except:
+        continue
