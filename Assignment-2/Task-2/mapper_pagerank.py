0c1ee18c728bf1f3d153ac42fcea686e64a90e05
Gituser143 <bhargavsnv100@gmail.com>
2020-09-21 23:56:33 +0530

    Optimise pagerank reducer

--- Assignment-2/Task-2/mapper_pagerank.py
+++ Assignment-2/Task-2/mapper_pagerank.py
@@ -2,23 +2,20 @@
 import sys
 
 dict = {}
-v_file = open("v", "r")
-nodes = v_file.readlines()
-for node_line in nodes:
-    v_node, rank = node_line.split(",")
+with open("v", "r") as v_file:
+    line = v_file.readline().strip()
+    v_node, rank = line.split(",")
     dict[v_node] = float(rank)
 
 for line in sys.stdin:
-    
     try:
         node, outlinks = line.split()
         outlinks = outlinks.split(",")
-        outlinks = [int(i) for i in outlinks]
+        rank = dict[node]
+        contribution = rank/len(outlinks)
 
-        for v_node in dict:
-            if v_node == node:
-                    for i in outlinks:
-                        print(i, dict[v_node]/len(outlinks))
-                        break
+        for i in outlinks:
+            print(i, contribution)
+
     except:
         continue
