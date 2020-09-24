eac560e54d1a299421c067dc2616ff184a8a19d4
Gituser143 <bhargavsnv100@gmail.com>
2020-09-24 12:53:02 +0530

    Attempt sorting dicts

--- Assignment-2/Task-1/reducer_adjlist.py
+++ Assignment-2/Task-1/reducer_adjlist.py
@@ -8,7 +8,7 @@ outlinks = []
 node = None
 
 with open(v_file_dest, "w") as file:
-    
+
     for line in sys.stdin:
         line = line.strip()
         node, outlink = line.split()
@@ -25,13 +25,16 @@ with open(v_file_dest, "w") as file:
             # for v file
             if node not in dict:
                 dict[node] = 1
-                file.write("%s,%d\n" % (node, 1))
+                # file.write("%s,%d\n" % (node, 1))
             if outlink not in dict:
                 dict[outlink] = 1
-                file.write("%s,%d\n" % (outlink, 1))
+                # file.write("%s,%d\n" % (outlink, 1))
         except:
             continue
 
-if current_node == node:
-    comma_sep_value = ",".join(outlinks)
-    print(current_node, comma_sep_value)
+    if current_node == node:
+        comma_sep_value = ",".join(outlinks)
+        print(current_node, comma_sep_value)
+
+    for key in dict.sort():
+        file.write("%s,%d\n" % (key, 1))
--- Assignment-2/Task-2/reducer_pagerank.py
+++ Assignment-2/Task-2/reducer_pagerank.py
@@ -14,6 +14,8 @@ for line in sys.stdin:
     except:
         continue
 
+# keys = dict.keys().sort()
+
 for key in dict.sort():
     new_rank = 0.15 + (0.85*dict[key])
     print("%s,%.5f" % (key, new_rank))
