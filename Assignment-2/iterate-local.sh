7f871adc9861dc25a054f743c28a2eaed25f801a
nsankethreddy <sankethn1947@gmail.com>
2020-10-03 11:40:07 +0530

    update iterate local script to print iteration number

--- Assignment-2/iterate-local.sh
+++ Assignment-2/iterate-local.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 CONVERGE=1
+COUNT=0
 rm v*
 
 cat ../../web-Google.txt | ./Task-1/mapper_adjlist.py | sort -k1,1 | ./Task-1/reducer_adjlist.py './v' > adjlist #has adjacency list
@@ -9,6 +10,6 @@ while [ "$CONVERGE" -ne 0 ]
 do
 	cat adjlist | ./Task-2/mapper_pagerank.py './v' | sort -k1,1 | ./Task-2/reducer_pagerank.py > v1
 	CONVERGE=$(python3 check_conv.py >&1)
-	echo $CONVERGE
-
+	COUNT=`expr $COUNT + 1`
+	echo "iteration: $COUNT"
 done
