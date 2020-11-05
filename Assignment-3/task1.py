a4abded4cc39a80b16ee4e21747e2bb38121e97c
nsankethreddy <sankethn1947@gmail.com>
2020-11-05 17:33:53 +0530

    loading data in spark

---
+++ Assignment-3/task1.py
@@ -0,0 +1,14 @@
+from pyspark import SparkContext, SparkConf
+import sys
+
+conf = SparkConf().setAppName("ShapeStrokes").setMaster("local")
+sc = SparkContext(conf=conf)
+
+word = sys.argv[1]
+dataset1 = sys.argv[2]
+dataset2 = sys.argv[3]
+
+shapeRDD = sc.textFile(dataset2)
+dict = shapeRDD.collect()
+
+print(dict)
