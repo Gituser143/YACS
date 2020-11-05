b469a98052c1dd976d5a462300bf34b43e06f936
Abhishek262 <abhishek262014@gmail.com>
2020-11-05 19:14:24 +0530

    StructType error to be fixed

--- Assignment-3/task1.py
+++ Assignment-3/task1.py
@@ -1,6 +1,11 @@
 from pyspark import SparkContext, SparkConf
-
+import pyspark
 import sys
+from pyspark.sql.types import StructType
+from pyspark.sql.types import StructField
+from pyspark.sql.types import StringType
+from pyspark.sql.types import IntegerType
+# from pyspark.sql.
 
 conf = SparkConf().setAppName("ShapeStrokes").setMaster("local")
 sc = SparkContext(conf=conf)
@@ -10,9 +15,27 @@ dataset1 = sys.argv[2]
 dataset2 = sys.argv[3]
 
 shapeRDD = sc.textFile(dataset2)
-dict = shapeRDD.collect()
+# dict = shapeRDD.collect()
 
-wordlist = shapeRDD.filter(lambda line: word in line.lower())
+wordlistRDD = shapeRDD.filter(lambda line: word in line.lower())
+# lst = []
+# for line in wordlistRDD.collect():
+#     line.split(",")
+#     lst.append(line)
 
+# rdd = sc.parallelize(lst)
 
-print(wordlist.collect())
+sql = pyspark.SQLContext(sc)
+schema = StructType([StructField("word", StringType(), True),StructField("timestamp",StringType(),True),
+StructField("reccognized",StringType(),True),StructField("key_id",IntegerType(),True),
+StructField("Total_Strokes",IntegerType(),True)])
+
+df = sql.createDataFrame(data=wordlistRDD,schema=schema)
+print(df.schema)
+# x = wordlistRDD.filter(wordlistRDD['reccognized'] == True).agg({"Total_Strokes": "avg"})
+# print(x)
+# word_df = wordlistRDD.toDF()
+# word_df.printSchema()
+# print(wordlist.collect())
+#round answer to 5 digits
+#if word is not present output 0
