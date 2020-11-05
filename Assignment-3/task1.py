dde654b279e1fa10e755de169adeb3826a473e9d
Abhishek262 <abhishek262014@gmail.com>
2020-11-05 22:04:42 +0530

    i think this works?

--- Assignment-3/task1.py
+++ Assignment-3/task1.py
@@ -11,31 +11,33 @@ conf = SparkConf().setAppName("ShapeStrokes").setMaster("local")
 sc = SparkContext(conf=conf)
 
 word = sys.argv[1]
-dataset1 = sys.argv[2]
+dataset1 = sys.argv[2]#not really needed
 dataset2 = sys.argv[3]
 
 shapeRDD = sc.textFile(dataset2)
+# print(shapeRDD.collect())
 # dict = shapeRDD.collect()
 
 wordlistRDD = shapeRDD.filter(lambda line: word in line.lower())
-# lst = []
-# for line in wordlistRDD.collect():
-#     line.split(",")
-#     lst.append(line)
 
-# rdd = sc.parallelize(lst)
 
 sql = pyspark.SQLContext(sc)
-schema = StructType([StructField("word", StringType(), True),StructField("timestamp",StringType(),True),
-StructField("reccognized",StringType(),True),StructField("key_id",IntegerType(),True),
-StructField("Total_Strokes",IntegerType(),True)])
+schema = StructType([StructField("word", StringType(), True),
+    StructField("timestamp",StringType(),True),
+    StructField("reccognized",StringType(),True),
+    StructField("key_id",StringType(),True),
+    StructField("Total_Strokes",StringType(),True)])
 
-df = sql.createDataFrame(data=wordlistRDD,schema=schema)
-print(df.schema)
-# x = wordlistRDD.filter(wordlistRDD['reccognized'] == True).agg({"Total_Strokes": "avg"})
-# print(x)
+df = sql.createDataFrame(wordlistRDD.map(lambda s: s.split(",")),schema=schema)
+print(df.count())
+x = df.filter(df['reccognized'] == False).agg({"Total_Strokes": "avg"})
+x.show()
+y = df.filter(df['reccognized'] == True).agg({"Total_Strokes": "avg"})
+y.show()
 # word_df = wordlistRDD.toDF()
 # word_df.printSchema()
 # print(wordlist.collect())
+
+
 #round answer to 5 digits
 #if word is not present output 0
