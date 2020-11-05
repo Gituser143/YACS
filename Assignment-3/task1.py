d8869032faccb2c65981236511416ad4d1633374
Abhishek262 <abhishek262014@gmail.com>
2020-11-05 22:32:40 +0530

    formatting

--- Assignment-3/task1.py
+++ Assignment-3/task1.py
@@ -7,20 +7,23 @@ from pyspark.sql.types import StringType
 from pyspark.sql.types import IntegerType
 # from pyspark.sql.
 
+#load up the SparkContext object
 conf = SparkConf().setAppName("ShapeStrokes").setMaster("local")
 sc = SparkContext(conf=conf)
 
+#argument parsing
 word = sys.argv[1]
 dataset1 = sys.argv[2]#not really needed
 dataset2 = sys.argv[3]
-
+#init RDD
 shapeRDD = sc.textFile(dataset2)
 # print(shapeRDD.collect())
 # dict = shapeRDD.collect()
 
+#RDD with only required word
 wordlistRDD = shapeRDD.filter(lambda line: word in line.lower())
 
-
+#Converting RDD to dataframe
 sql = pyspark.SQLContext(sc)
 schema = StructType([StructField("word", StringType(), True),
     StructField("timestamp",StringType(),True),
@@ -29,10 +32,12 @@ schema = StructType([StructField("word", StringType(), True),
     StructField("Total_Strokes",StringType(),True)])
 
 df = sql.createDataFrame(wordlistRDD.map(lambda s: s.split(",")),schema=schema)
-print(df.count())
+
+#Required answers
 x = df.filter(df['reccognized'] == False).agg({"Total_Strokes": "avg"})
-x.show()
+x.show()# print(format(x[0]))
 y = df.filter(df['reccognized'] == True).agg({"Total_Strokes": "avg"})
+print(y[0])
 y.show()
 # word_df = wordlistRDD.toDF()
 # word_df.printSchema()
