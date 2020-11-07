39d4b625be622d08763757aaf260504db8898251
nsankethreddy <sankethn1947@gmail.com>
2020-11-07 11:21:22 +0530

    Cleaned task1

--- Assignment-3/task1.py
+++ Assignment-3/task1.py
@@ -1,44 +1,41 @@
-from pyspark import SparkContext, SparkConf
-import sys
+from pyspark import SparkContext
 from pyspark.sql.types import StructType
 from pyspark.sql.types import StructField
 from pyspark.sql.types import StringType
 from pyspark.sql.types import IntegerType
 from pyspark import SQLContext
-#load up the SparkContext object
-conf = SparkConf().setAppName("ShapeStrokes").setMaster("local")
-sc = SparkContext(conf=conf)
+import sys
 
-#argument parsing
+# load up the SparkContext object
+sc = SparkContext()
+
+# argument parsing
 word = sys.argv[1]
-dataset1 = sys.argv[2]#not really needed
+dataset1 = sys.argv[2]
 dataset2 = sys.argv[3]
-#init RDD
-shapeRDD = sc.textFile(dataset2)
 
-#RDD with only required word
-wordlistRDD = shapeRDD.filter(lambda line: word in line.lower())
+# init RDD
+shape_statRDD = sc.textFile(dataset2)
 
-#Converting RDD to dataframe
+# RDD with only required word
+wordlistRDD = shape_statRDD.filter(lambda line: word in line.lower())
+
+# Converting RDD to dataframe
 sql = SQLContext(sc)
 schema = StructType([StructField("word", StringType(), True),
-    StructField("timestamp",StringType(),True),
-    StructField("reccognized",StringType(),True),
-    StructField("key_id",StringType(),True),
-    StructField("Total_Strokes",StringType(),True)])
+                     StructField("timestamp", StringType(), True),
+                     StructField("reccognized", StringType(), True),
+                     StructField("key_id", StringType(), True),
+                     StructField("Total_Strokes", StringType(), True)])
 
-df = sql.createDataFrame(wordlistRDD.map(lambda s: s.split(",")),schema=schema)
+df = sql.createDataFrame(wordlistRDD.map(lambda s: s.split(",")), schema=schema)
 
-#Required answers
+# Required answers in dataframe
 x = df.filter(df['reccognized'] == False).agg({"Total_Strokes": "avg"})
-x.show()# print(format(x[0]))
+x.show()
 y = df.filter(df['reccognized'] == True).agg({"Total_Strokes": "avg"})
-print(y[0])
 y.show()
-# word_df = wordlistRDD.toDF()
-# word_df.printSchema()
-# print(wordlist.collect())
 
 
-#round answer to 5 digits
-#if word is not present output 0
+# round answer to 5 digits
+# if word is not present output 0
