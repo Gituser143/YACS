b3ecb6f7629d46e23407534663920bc1f4870e52
Gituser143 <bhargavsnv100@gmail.com>
2020-11-21 21:02:57 +0530

    Init worker listener

--- YACS/src/master.py
+++ YACS/src/master.py
@@ -23,36 +23,41 @@ conf_path = sys.argv[1]
 sched_algo = sys.argv[2]
 sched_algo = sched_algo.upper()
 
-# Load JSON config
-config_file = open(conf_path, "r")
-json_data = json.load(config_file)
+# Metatdata
 
-# Get information of workers
-workers = json_data["workers"]
-
-# Stats holds information on each worker
-# Format:
-#   worker_id: [total_slots, slots_available, [ip, port]]
+'''
+Stats holds information on each worker
+Format:
+  worker_id: [total_slots, slots_available, [ip, port]]
+'''
 stats = dict()
 
-# Holds list of map and dependent reduce tasks for a given job
-# Format:
-#   jobId: [[map tasks][reduce tasks]]
+'''
+Holds list of map and dependent reduce tasks for a given job
+Format:
+  jobId: {map: {task_id: map task}, reduce: {task_id: reduce task}}
+'''
 task_dependencies = dict()
 
-# Mutex to be used when modifying data
+# Variables for Least-Loaded scheduling
+
+'''
+Holds current free slots for workers
+Format:
+  num_free_slots: [workers with said free slots]
+'''
+loads = dict()
+
+# Mutexes to be used when modifying data
 task_mutex = threading.Lock()
 stats_mutex = threading.Lock()
 
-# Variables for Least-Loaded
-
-# Holds current free slots for workers
-# Format:
-#   num_free_slots: [workers with said free slots]
-loads = dict()
-
 
 def init_meta(stats, sched_algo):
+    '''
+    Function to Initialise metatda. Takes in the
+    scheduling algorithm chosen and a stats dictionary.
+    '''
     # Least loaded by default
     if sched_algo == "RANDOM":
         pass
@@ -68,62 +73,84 @@ def init_meta(stats, sched_algo):
                 loads[free_slots] = [worker]
 
 
-def send_job(worker_id, task):
+def send_job(worker_id, job_id, task):
+    '''
+    Given a worker and a task, the function sends
+    the task to the worker through a socket.
+    '''
+
     # Get socket details of specified worker
     worker_ip, worker_port = stats[worker_id][2]
+
+    # Create message to send
+    message = {"job_id": job_id, "task": task}
 
     # Open socket connection
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((worker_ip, worker_port))
-        message = json.dumps(task)
+        message = json.dumps(str(message))
 
         # Send Task
         s.send(message.encode())
 
 
-def schedule_job(job_ID, sched_algo):
+def schedule_job(job_id, sched_algo, task_type="map"):
+    '''
+    Given a job ID and a scheduling algorithm,
+    the function schedules all map tasks in
+    the given job on workers according to the
+    scheduing algorithm.
+    '''
+
+    # Get Job to run
+    task_mutex.acquire()
+    job = task_dependencies[job_id]
+    task_mutex.release()
+
+    # Get tasks with no dependencies
+    tasks = job[task_type]
+
     if sched_algo == "RANDOM":
         pass
     elif sched_algo == "RR":
         pass
     else:
-
-        # Get Job to run
-        task_mutex.acquire()
-        job = task_dependencies[job_ID]
-        task_mutex.release()
-
-        map_tasks = job[0]
-
-        for task in map_tasks:
+        for task_id in tasks:
+            task = tasks[task_id]
 
             # Ensure empty slots are available
             has_empty_slots.acquire()
 
-            # Get least load
+            # Critical section to update metatdata
             stats_mutex.acquire()
-            least_load = max(loads)
-            stats_mutex.release()
 
-            # Get least loaded worker
-            stats_mutex.acquire()
+            # Get least load
+            least_load = max(loads)
+
             # Get least loaded worker
             worker_id = loads[least_load].pop(0)
             if len(loads[least_load]) == 0:
                 del loads[least_load]
-
-            # Send task to worker
-            send_job(worker_id, task)
 
             # Update free slots metatdata
             if least_load-1 in loads:
                 loads[least_load-1].append(worker_id)
             else:
                 loads[least_load-1] = [worker_id]
+
             stats_mutex.release()
+
+            # Send task to worker
+            send_job(worker_id, job_id, task)
 
 
 def client_listener(n):
+    '''
+    Function which listens to job submissions
+    from the client. Extracts information and
+    proceeds to update metadata and schedule job.
+    '''
+
     # Initialise socket
     server_ip = "localhost"
     client_port = 5000
@@ -152,9 +179,12 @@ def client_listener(n):
         map_tasks = job_data["map_tasks"]
         reduce_tasks = job_data["reduce_tasks"]
 
+        map_dict = {task["task_id"]: task for task in map_tasks}
+        reduce_dict = {task["task_id"]: task for task in reduce_tasks}
+
         # Add job to list of task dependencies
         task_mutex.acquire()
-        task_dependencies[job_id] = [map_tasks, reduce_tasks]
+        task_dependencies[job_id] = {"map": map_dict, "reduce": reduce_dict}
         task_mutex.release()
 
         # Schedule job on workers
@@ -162,6 +192,13 @@ def client_listener(n):
 
 
 def worker_listener(n):
+    '''
+    Function which listens to task completions
+    from the client. Extracts information and
+    proceeds to update metadata and schedule
+    dependent tasks if any.
+    '''
+
     # =====================================
     # TODO: Multithread the worker listener
     # =====================================
@@ -185,16 +222,61 @@ def worker_listener(n):
             if not data:
                 break
             message += data
-        print(message)
+
+        message = message.decode()
+        response = json.loads(message)
+
+        # Extract data from response
+        job_id = response["job_id"]
+        task_id = response["task_id"]
+        task_type = response["task_type"]
+        worker_id = response["worker_id"]
 
         # ========================================
         # TODO: Update task_dependencies and meta
         # ========================================
+        task_mutex.acquire()
 
-        # ===================================================
-        # TODO: Schedule Reduce task if all map have finished
-        # ===================================================
+        # Remove job from dependencies
+        del task_dependencies[job_id][task_type][task_id]
 
+        task_mutex.release()
+
+        if sched_algo == "RR":
+            pass
+        elif sched_algo == "RANDOM":
+            pass
+        else:
+            stats_mutex.acquire()
+
+            # Update free slots for worker
+            for free_slots in loads:
+                if worker_id in loads[free_slots]:
+                    loads[free_slots].remove(worker_id)
+
+                    if free_slots+1 in loads:
+                        loads[free_slots+1].append(worker_id)
+                    else:
+                        loads[free_slots+1] = [worker_id]
+
+            stats_mutex.release()
+
+        # If no more map tasks exist, schedule reduce tasks
+        if task_type == "map":
+            task_mutex.acquire()
+            remaining_map_tasks = len(task_dependencies[job_id][task_type])
+            task_mutex.release()
+
+            if remaining_map_tasks == 0:
+                schedule_job(job_id, sched_algo, "reduce")
+
+
+# Load JSON config
+config_file = open(conf_path, "r")
+json_data = json.load(config_file)
+
+# Get information of workers
+workers = json_data["workers"]
 
 total_slots = 0
 
