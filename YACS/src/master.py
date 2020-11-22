f527190cf323e8bd534b7704f41264111359ca4d
Akshatha Laxmi <akshata.lakshmi@gmail.com>
2020-11-22 21:59:55 +0530

    Add Random Scheduler

--- YACS/src/master.py
+++ YACS/src/master.py
@@ -3,6 +3,7 @@ import os
 import json
 import socket
 import threading
+import random
 
 # Verifying command line arguments
 if len(sys.argv) < 3:
@@ -41,13 +42,66 @@ for worker in workers:
     # Create worker processes
     pid = os.fork()
     if pid == -1:
-        print("Faile to spawn worker {id}".format(id=worker_id))
+        print("Failed to spawn worker {id}".format(id=worker_id))
     elif pid == 0:
         os.execv("worker.py", args)
+print(stats)
 
 n = len(workers)
 print("Spawned {n} workers".format(n=n))
 
+def send_request(task, address):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((address[0], address[1]))
+        message = json.dumps(task)
+        s.send(message.encode())
+
+def scheduler(job):
+
+    while job["status"] != 0:
+        if job["status"] == 2:
+            map_tasks = job["map_tasks"]
+            if sched_algo == 'RANDOM':
+                random_sched(map_tasks, job["job_id"], "M")
+            elif sched_algo == 'RR':
+                # round_robin_sched(map_tasks)
+                print("RR")
+            else:
+                # least_loaded_sched(map_tasks)
+                print("LL")
+            
+            # job["status"] = 1
+        if job["status"] == 1:
+            reduce_tasks = job["reduce_tasks"]
+            if sched_algo == 'RANDOM':
+                random_sched(reduce_tasks, job["job_id"], "R")
+            elif sched_algo == 'RR':
+                # round_robin_sched(reduce_tasks)
+                print("RR")
+            else:
+                # least_loaded_sched(reduce_tasks)
+                print("LL")
+
+def random_sched(job_id, task_type):
+    tasks = jobs["job_id"]
+    for task in tasks:
+        chosen_worker = random.choice(list(stats))
+
+        # put semaphore here instead of while loop
+        while stats[chosen_worker][1] == 0:
+            chosen_worker = random.choice(list(stats))
+
+        task["type"] = task_type
+        task["job_id"] = job_id
+        stats[chosen_worker][1] -= 1
+        send_request(task, stats[chosen_worker][2])
+
+def round_robin_sched(job_id, task_type):
+
+    worker_ids = list(stats)
+
+
+jobs = dict()
 
 def client_listener(n):
     # Initialise socket
@@ -55,6 +109,7 @@ def client_listener(n):
     client_port = 5000
 
     print("Inside thread")
+
 
     # Bind socket for client interactions
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -71,12 +126,27 @@ def client_listener(n):
             if not data:
                 break
             message += data
-        print(message)
+        # print(message)
 
-    # ======================================
-    # TODO: Process job details and schdeule
-    # ======================================
+        # ======================================
+        # Process job details and schdeule
+        # ======================================
 
+        message = message.decode("utf-8")
+        json_job = json.loads(message)
+        # status -> 2 => incomplete map and reduce, 
+        # status -> 1 => complete map, incomplete red, 
+        # status -> 0 => complete map and reduce
+        # completed -> [0, 0] => 0 completed map tasks, 0 completed reduce tasks
+        jobs[json_job["job_id"]] = {"job_id": json_job["job_id"], \
+                                    "map_tasks": json_job["map_tasks"], \
+                                    "reduce_tasks": json_job["reduce_tasks"], \
+                                    "status": 2, \
+                                    "completed": [0, 0]\
+                                    }
+        print(jobs)
+
+        scheduler(jobs[json_job["job_id"]])
 
 def worker_listener(n):
     # =====================================
@@ -95,17 +165,30 @@ def worker_listener(n):
     worker.listen(n)
     while True:
         sock, address = worker.accept()
-
+        print("Got connection from ", address)
         message = bytes()
         while True:
             data = sock.recv(1024)
             if not data:
                 break
             message += data
-        print(message)
         # =====================================
         # TODO: Update metadata on completions
         # =====================================
+
+        message = message.decode("utf-8")
+        worker_response = json.loads(message)
+
+        stats[int(worker_response["worker_id"])][1] += 1
+
+        if worker_response["type"] == 'M':
+            jobs[worker_response["job_id"]]["completed"][0] += 1
+            if jobs[worker_response["job_id"]]["completed"][0] == len(jobs[worker_response["job_id"]]["map_tasks"]):
+                jobs[worker_response["job_id"]]["status"] = 1
+        elif worker_response["type"] == 'R':
+            jobs[worker_response["job_id"]]["completed"][1] += 1
+            if jobs[worker_response["job_id"]]["completed"][1] == len(jobs[worker_response["job_id"]]["reduce_tasks"]):
+                jobs[worker_response["job_id"]]["status"] = 0
 
 
 # Create threads to listen for clients and workers
--- YACS/src/worker.py
+++ YACS/src/worker.py
@@ -2,14 +2,76 @@
 
 import sys
 import time
+import socket
+import json
+import threading
 
 if len(sys.argv) < 3:
     print("ERROR: Not Enough Arguments")
     print("Usage:", sys.argv[0], "PORT WORKER_ID")
     exit(1)
 
-port = sys.argv[1]
+port = int(sys.argv[1])
 id = sys.argv[2]
+server_ip = "localhost"
 
 time.sleep(1)
 print("Spawned worker {id} on port {p}".format(p=port, id=id))
+
+execution_pool = []
+
+def master_listener():
+
+    master = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    master.bind((server_ip, port))
+
+    master.listen()
+    while True:
+        sock, address = master.accept()
+        print("Got connection from", address)
+        message = bytes()
+
+        while True:
+            data = sock.recv(1)
+            if not data:
+                break
+            message += data
+
+        message = message.decode("utf-8")
+
+        task = json.loads(message)
+
+        execution_pool.append(task)
+
+def send_updates_to_master(task):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((server_ip, 5001))
+        message = json.dumps(task)
+        s.send(message.encode())
+
+def worker():
+
+    while True:
+        # infinite loop that waits for a task to be added to the execution pool
+        while len(execution_pool) == 0:
+            continue
+        
+        remaining_duration = execution_pool[0]["duration"]
+
+        while remaining_duration:
+            remaining_duration -= 1
+            if remaining_duration == 0:
+                task = execution_pool.pop(0)
+                task["remaining_duration"] = 0
+                task["worker_id"] = id
+                send_updates_to_master(task)
+
+
+master_listner_thread = threading.Thread(target=master_listener)
+worker_thread = threading.Thread(target=worker)
+
+master_listner_thread.start()
+worker_thread.start()
+
+master_listner_thread.join()
+worker_thread.join()
